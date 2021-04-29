bc92e47e7c0177bab8168bddeaf4db6ca9d73356
JayAreP <43276115+JayAreP@users.noreply.github.com>
2021-04-29 13:38:58 -0700

    Many changes concerning URI massaging for lt/gt queries

--- SDP/CoreOps/Get-SDPEvents.ps1
+++ SDP/CoreOps/Get-SDPEvents.ps1
@@ -1,3 +1,16 @@
+class sdpevent {
+    [string] $event_id
+    [string] $id
+    [string] $labels
+    [string] $level 
+    [string] $message
+    [string] $name
+    [datetime] $timestamp 
+    [string] $user
+    [string] $pipeId
+    [string] $pipeName
+}
+
 function Get-SDPEvents {
     param(
         [parameter()]
@@ -14,7 +27,7 @@ function Get-SDPEvents {
         [parameter()]
         [string] $name,
         [parameter()]
-        [datetime] $timestamp,
+        [datetime] $after,
         [parameter()]
         [string] $user,
         [parameter()]
@@ -43,14 +56,38 @@ function Get-SDPEvents {
 
     # function specific operations
     process {
-        if ($timestamp) {
-            $cdate = Convert-SDPTimeStampTo -timestamp $timestamp
-            $PSBoundParameters.remove('timestamp') | Out-Null
+        if ($after) {
+            $cdate = Convert-SDPTimeStampTo -timestamp $after -int
+            $PSBoundParameters.remove('after') | Out-Null
             $PSBoundParameters.timestamp = $cdate
         }
         
-        $results = Invoke-SDPRestCall -endpoint $endpoint -method GET -parameterList $PSBoundParameters -k2context $k2context -strictURI
-        return $results
+        $results = Invoke-SDPRestCall -endpoint $endpoint -method GET -parameterList $PSBoundParameters -k2context $k2context -strictURI -strictURIgte timestamp
+
+        $eventArray = @()
+
+        foreach ($i in $results) {
+            # Object
+            # Build an instance of the class
+            $classSDPEvent = [sdpevent]::new()
+
+            # Populate the class object
+            $classSDPEvent.event_id = $i.event_id
+            $classSDPEvent.id = $i.id
+            $classSDPEvent.labels = $i.labels
+            $classSDPEvent.level = $i.level
+            $classSDPEvent.message = $i.message
+            $classSDPEvent.name = $i.name
+            $classTimeStamp = Convert-SDPTimeStampFrom -timestamp $i.timestamp
+            $classSDPEvent.timestamp = $classTimeStamp
+            $classSDPEvent.user = $i.user
+            $classSDPEvent.pipeId = $i.pipeId
+            $classSDPEvent.pipeName = $i.pipeName
+
+            $eventArray += $classSDPEvent
+        }
+
+        return $eventArray
     }
 }
 
